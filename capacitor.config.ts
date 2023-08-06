4a175a6de1ca4553193f19531c97c865eeb82935
Saranga <54878558+Saranaga123@users.noreply.github.com>
2023-08-07 00:16:39 +0530

    landing page
    landing page

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sotlgotfol.app',
+  appName: 'sotlgotfol',
+  webDir: 'dist/sothe-last-guardian',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
