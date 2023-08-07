da47f1cad63215b4c3f6ad361dafcad384bab575
Saranga <54878558+Saranaga123@users.noreply.github.com>
2023-08-07 19:46:32 +0530

    fullscreen
    fullscreen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { StatusBar } from '@capacitor/status-bar';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SOTheLastGuardian';
+  constructor() {
+    // Call the hide() method of the StatusBar plugin to hide the status bar and go full-screen
+    StatusBar.hide();
+  }
 }
