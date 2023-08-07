e00c37f29285bdbb1e58b6d700e3cc727ecd1689
Saranga <54878558+Saranaga123@users.noreply.github.com>
2023-08-07 20:38:12 +0530

    fullscreenTestV2
    fullscreenTestV2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
-import { StatusBar } from '@capacitor/status-bar';
+import { Component, OnInit } from '@angular/core';
+import { Plugins, Capacitor } from '@capacitor/core';
+
+const { StatusBar, Platform } = Plugins;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'SOTheLastGuardian';
-  constructor() {
-    // Call the hide() method of the StatusBar plugin to hide the status bar and go full-screen
-    StatusBar.hide();
+export class AppComponent implements OnInit {
+  async ngOnInit() {
+    // Check the platform using the Platform plugin
+    const isAndroid = await Platform['is']('android');
+    if (isAndroid) {
+      // Use platform-specific code to hide both the status bar and navigation bar
+      // For example, you can use the following to set the app to full-screen immersive mode
+      const fullscreenOptions = { immersive: true, stickyImmersive: false };
+      StatusBar['setOverlaysWebView']({ overlays: true });
+      StatusBar['setFullscreen'](fullscreenOptions);
+    }
   }
 }
